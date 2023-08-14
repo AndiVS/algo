e61d3514341df331858860fd0b1535c344ce8b04
Andrei Saldyun <asaldyun@yandex.ru>
2023-08-14 19:51:22 +0300

    add tree

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/AndiVS/algo/tree"
+)
 
 func main() {
-	fmt.Print(insertionSort([]int{5, 3, 6, 1}))
+	fmt.Print(tree.DfsSum(&tree.Node{
+		Val: 1,
+		R: &tree.Node{
+			Val: 2,
+			R: &tree.Node{
+				Val: 3,
+				R:   nil,
+				L:   nil,
+			},
+			L: &tree.Node{
+				Val: 3,
+				R:   nil,
+				L:   nil,
+			},
+		},
+		L: nil,
+	}))
 }
